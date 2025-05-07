183e5b3f1ccafb68d5d06ec274cea2cbdff37699
sergss <sergey809296@yandex.ru>
2025-05-07 17:12:25 +0300

    classification model and template added

---
+++ filmreviews/sentimentmodel/bert/bert_model.py
@@ -0,0 +1,38 @@
+import os
+import torch
+
+from django.conf import settings
+from transformers import BertTokenizer, BertForSequenceClassification
+from sentimentmodel.bert.text_preprocessing import normalize_text
+
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+weights_path = os.path.join(settings.BASE_DIR, 'sentimentmodel',
+                            'bert', 'weights', '2', 'bsd2.pt')
+model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2).to(DEVICE)
+
+st = torch.load(weights_path)
+model.load_state_dict(state_dict=st)
+model.eval()
+
+
+def classificate(text):
+    prepared_seq = normalize_text(text)
+
+    encoded_dict = tokenizer.encode_plus(
+        prepared_seq,  # текст строка которую кодируем
+        add_special_tokens=True,  # добавить '[CLS]' и '[SEP]' токены
+        max_length=512,  # параметр максимальной длины текста
+        padding='max_length',  # делать падинг до макс длины
+        truncation=True,  # если длина больше max_length то отрезать лишнее
+        return_attention_mask=True,  # делать ли маску внимания
+        return_tensors='pt',  # формат возвращаемого тензора
+        # return_token_type_ids=False,
+    )
+    input_ids = encoded_dict['input_ids'].to(DEVICE)
+    attention_mask = encoded_dict['attention_mask'].to(DEVICE)
+
+    outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+    mark = torch.argmax(outputs.logits).item()
+    return str(mark)
--- filmreviews/sentimentmodel/views.py
+++ filmreviews/sentimentmodel/views.py
@@ -1,11 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.loader import render_to_string
+from .bert.bert_model import classificate
 
 
 # Create your views here.
 def model_view(request):
-    return render(request, 'sentimentmodel/model_page.html')
+    # 'Such a good film. Brilliant actors. Beautifull locations'
+    # "That was awfull. Horrible cast. Lousy scenario. Don't waste your time."
+    data = {
+        'mark': classificate("That was awfull. Horrible cast. Lousy scenario. Don't waste your time.")
+    }
+    return render(request, 'sentimentmodel/model_page.html', context=data)
 
 
 def home_page_view(request):
