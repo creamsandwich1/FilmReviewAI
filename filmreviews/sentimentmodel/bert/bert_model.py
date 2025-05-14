6cedb45a3afbf102466b50621f0126f48a1bed64
sergss <sergey809296@yandex.ru>
2025-05-14 20:31:05 +0300

    translation added and downloading weights from drive

--- filmreviews/sentimentmodel/bert/bert_model.py
+++ filmreviews/sentimentmodel/bert/bert_model.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import gdown
 
 from django.conf import settings
 from transformers import BertTokenizer, BertForSequenceClassification
@@ -9,10 +10,14 @@ DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
 weights_path = os.path.join(settings.BASE_DIR, 'sentimentmodel',
-                            'bert', 'weights', '2', 'bsd2.pt')
+                            'bert', 'weights', '3', 'bsd1.pt')
 model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2).to(DEVICE)
 
-st = torch.load(weights_path)
+if not os.path.exists(weights_path):
+    url = 'https://drive.google.com/file/d/1fcMJjIhsAS_jQfviilofvepItxyHQcdb/view?usp=drive_link'
+    gdown.download(url, weights_path, quiet=False, fuzzy=True)
+
+st = torch.load(weights_path, weights_only=False)
 model.load_state_dict(state_dict=st)
 model.eval()
 
